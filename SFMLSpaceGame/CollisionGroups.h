8a89fe2e2d7e5eb5e36789fde0ab15c298596d6d
deanljohnson <dean@deanljohnson.com>
2016-10-08 03:23:19 -0700

    Comments

--- SFMLSpaceGame/CollisionGroups.h
+++ SFMLSpaceGame/CollisionGroups.h
@@ -2,6 +2,14 @@
 
 const int BULLET_COLLISION_GROUP = -1;
 
+// these define bitmasks to be used in Box2d
+// for collision filtering. The IS_XXX apply
+// to a fixture's category masks and they define
+// what an object is. The COLLIDES_WITH_XXX
+// define what categories an object should
+// collide with and should be assigned to a
+// fixtures mask bits.
+
 const int IS_SHIP =					0x0001;
 const int IS_BULLET =				0x0002;
 const int IS_STATION =				0x0004;
