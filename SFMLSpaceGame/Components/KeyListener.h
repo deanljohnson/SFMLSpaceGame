72833a60871776189843fd7d38fb908d1d6029c2
deanljohnson <dean@deanljohnson.com>
2016-10-17 03:04:36 -0700

    Spacing Fix

--- SFMLSpaceGame/Components/KeyListener.h
+++ SFMLSpaceGame/Components/KeyListener.h
@@ -19,5 +19,6 @@ public:
 	{}
 
 	virtual void Update() override;
+
 	void operator +=(std::function<void(sf::Keyboard::Key)> callback);
 };
--- SFMLSpaceGame/ExtendedMath.h
+++ SFMLSpaceGame/ExtendedMath.h
@@ -33,7 +33,7 @@ inline sf::FloatRect Union(const sf::FloatRect& a, const sf::FloatRect& b)
 	float top = std::min(a.top, b.top);
 	float right = std::max((a.left + a.width), (b.left + b.width));
 	float bottom = std::max((a.top + a.height), (b.top+ b.height));
-
+	
 	return sf::FloatRect(left, top, right - left, bottom - top);
 }
 
