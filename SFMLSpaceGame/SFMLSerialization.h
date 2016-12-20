ae00f889a843a59d551e29fb227b2b74704151db
deanljohnson <dean@deanljohnson.com>
2016-12-19 22:04:50 -0800

    Added world file

--- SFMLSpaceGame/SFMLSerialization.h
+++ SFMLSpaceGame/SFMLSerialization.h
@@ -1,21 +1,22 @@
 #pragma once
-#include <cereal\cereal.hpp>
+#include <cereal/cereal.hpp>
 #include <SFML/System/Vector2.hpp>
 
 // Must be placed in cereal namespace in order to be found by cereal
 namespace cereal {
 	template<class Archive>
-	void serialize(Archive& archive, sf::Vector2f& v)
+	void serialize(Archive& ar, sf::Vector2f& v)
 	{
-		archive(CEREAL_NVP(v.x), CEREAL_NVP(v.y));
+		ar(cereal::make_nvp("x", v.x), 
+			cereal::make_nvp("y", v.y));
 	}
 
 	template<class Archive>
-	void serialize(Archive& archive, sf::IntRect& v)
+	void serialize(Archive& ar, sf::IntRect& v)
 	{
-		archive(cereal::make_nvp("Left", v.left),
-				cereal::make_nvp("Top", v.top),
-				cereal::make_nvp("Width", v.width),
-				cereal::make_nvp("Height", v.height));
+		ar(cereal::make_nvp("Left", v.left),
+			cereal::make_nvp("Top", v.top),
+			cereal::make_nvp("Width", v.width),
+			cereal::make_nvp("Height", v.height));
 	}
 }
